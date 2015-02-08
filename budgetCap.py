542480c13bbc0bb3a22580f452a304d3d76b8220
Steven Whitehead <scw007@vt.edu>
2015-02-07 22:49:08 -0500

    poop

--- budgetCap.py
+++ budgetCap.py
@@ -30,24 +30,30 @@ def accountTotals():
         if(index["type"] == "checking"):
             acct1Total = acct1Total + index["balance"]
             billAcct1 = ('http://api.reimaginebanking.com/accounts/' + index["_id"] + '/bills?key=' + custKey)
+            acctTotal.append(acct1Total)
         else:
             acct1Total = acct1Total + index["balance"]
+            acctTotal.append(acct1Total)
   
     if(index["nickname"] == "Pedro"):
         acctTotal.append(index["nickname"])
         if(index["type"] == "checking"):
             acct2Total = acct2Total + index["balance"]
             billAcct2 = ('http://api.reimaginebanking.com/accounts/' + index["_id"] + '/bills?key=' + custKey)
+            acctTotal.append(acct2Total)
         else:
             acct2Total = acct2Total + index["balance"]
+            acctTotal.append(acct2Total)
   
     if(index["nickname"] == "Steven"):
         acctTotal.append(index["nickname"])
         if(index["type"] == "checking"):
             acct3Total = acct3Total + index["balance"]
+            acctTotal.append(acct3Total)
             billAcct3 = ('http://api.reimaginebanking.com/accounts/' + index["_id"] + '/bills?key=' + custKey)
         else:
             acct3Total = acct3Total + index["balance"]
+            acctTotal.append(acct3Total)
   
   print acct1Total
   print acct2Total
@@ -83,10 +89,10 @@ def index_lulu():
     nquestions=5
     tot=accountTotals()
     print tot
-    accountTotalOne=tot[7]
+    accountTotalOne=tot[1]
 
     if request.method == 'GET':
-      return render_template('steven.html',num=nquestions, account=accountTotalOne, Person=tot[0],)
+      return render_template('steven.html',num=nquestions, account=accountTotalOne, Person=tot[0],Income=50,Checking=tot[1],Savings=tot[3])
     else:
         #request was a POST
         app_lulu.vars['name'] = request.form['name_lulu']
